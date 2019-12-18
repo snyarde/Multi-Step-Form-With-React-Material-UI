0ddb28b2995d0def5fffe0ac62fe4766b8b58404
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-18 07:54:44 -0500

    Adds App component
    Adds App component to create base project structure. Removes default css.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import UserForm from "./components/UserForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <UserForm /> */}
+        <h1>Yo</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
